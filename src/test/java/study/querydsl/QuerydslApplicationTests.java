1191a9b7139cc0a2a440cf860e27e37103233060
kiyoung85 <kiyoung85@gmail.com>
2020-10-15 03:44:48 +0900

    Initial commit

---
+++ src/test/java/study/querydsl/QuerydslApplicationTests.java
@@ -0,0 +1,39 @@
+package study.querydsl;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.entity.Hello;
+import study.querydsl.entity.QHello;
+
+import javax.persistence.EntityManager;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class QuerydslApplicationTests {
+
+    @Autowired
+    EntityManager entityManager;
+
+    @Test
+    void contextLoads() {
+        Hello hello = new Hello();
+        entityManager.persist(hello);
+
+        JPAQueryFactory query = new JPAQueryFactory(entityManager);
+        QHello qHello = QHello.hello;
+
+        Hello fetchOne = query.selectFrom(qHello)
+                .fetchOne();
+
+        assertThat(fetchOne).isEqualTo(hello);
+        assertThat(fetchOne.getId()).isEqualTo(hello.getId());
+    }
+
+}
