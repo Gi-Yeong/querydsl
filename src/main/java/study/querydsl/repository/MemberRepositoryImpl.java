3bb9c09adc34de4bb3933be3ed15977ab6d23413
kiyoung85 <kiyoung85@gmail.com>
2020-10-20 01:03:03 +0900

    스프링 데이터 페이징 활용1 - Querydsl 최적화

--- src/main/java/study/querydsl/repository/MemberRepositoryImpl.java
+++ src/main/java/study/querydsl/repository/MemberRepositoryImpl.java
@@ -2,15 +2,18 @@ package study.querydsl.repository;
 
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.support.PageableExecutionUtils;
 import org.springframework.util.StringUtils;
 import study.querydsl.dto.MemberSearchCondition;
 import study.querydsl.dto.MemberTeamDto;
 import study.querydsl.dto.QMemberTeamDto;
+import study.querydsl.entity.Member;
 
 import java.util.List;
 
@@ -86,17 +89,21 @@ public class MemberRepositoryImpl implements MemberRepositoryCustom {
 
         // total count 를 가져올 때, content 쿼리와 다르게 count 쿼리는 심플하게
         // 만들 수 있다면, searchPageSimple 보다 join 등 심플하게 만들어서 하는게 좋을 수도 있다.
-        long count = queryFactory.select(member)
+        JPAQuery<Member> countQuery = queryFactory.select(member)
                 .from(member)
                 .leftJoin(member.team, team)
                 .where(usernameEq(condition.getUsername()),
                         teamNameEq(condition.getTeamName()),
                         ageGoe(condition.getAgeGoe()),
                         getLoe(condition.getAgeLoe())
-                )
-                .fetchCount();
+                );
 
-        return new PageImpl<>(content, pageable, count);
+        // -스프링 데이터 라이브러리가 제공
+        // - count 쿼리가 생략 가능한 경우 생략해서 처리
+        //   - 페이지 시작이면서 컨텐츠 사이즈가 페이지 사이즈보다 작을 때
+        //   - 마지막 페이지 일 때 (offset + 컨텐츠 사이즈를 더해서 전체 사이즈 구함)
+        return PageableExecutionUtils.getPage(content, pageable, () -> countQuery.fetchCount());
+//        return new PageImpl<>(content, pageable, count);
     }
 
     private BooleanExpression usernameEq(String username) {
